fb28e6504e1dcd9da51d96410e5c9c660c607667
Husain Fazal <husain.fazal@ontario.ca>
2015-05-07 16:02:51 -0400

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+Cordova ADFS Auth Dialog demo
+=============================
+Based on [Cordova 5.0](https://github.com/apache/cordova-lib/releases/tag/5.0.0), [Cordova Android 4.0](https://github.com/apache/cordova-android/releases/tag/4.0.0), [Cordova In App Browser 1.0 (with customization)](https://github.com/adrianchia/cordova-plugin-inappbrowser) and MSOpenTech's [cordova-plugin-auth-dialog](https://github.com/msopentech/cordova-plugin-auth-dialog).
+
+This example demostrates ADFS auth dialog with cordova. 
+
+**Note** Please change ADFS URL in www/js/index.js
---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,25 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.0.1-dev",
+    "com.msopentech.authdialog": "0.1.0-dev",
+    "cordova-plugin-inappbrowser": "1.0.1-dev"
+}
+// BOTTOM OF METADATA
+});
